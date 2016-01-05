3fa5796f38314a1d3cdc6913d69d1e62691719c1
ricardocoimbra01 <ricardocoimbra01@hotmail.com>
2016-01-05 22:54:57 +0000

    A

--- app/src/main/java/tmen/memorygame/Activities/EscolheTemaActivity.java
+++ app/src/main/java/tmen/memorygame/Activities/EscolheTemaActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -79,6 +82,16 @@ public class EscolheTemaActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
